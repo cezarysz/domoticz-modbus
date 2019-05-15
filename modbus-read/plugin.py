bd84e1ea4ee54775fa420737ee7f1671968cb7a4
cezarysz <48601376+cezarysz@users.noreply.github.com>
2019-05-15 13:21:01 +0200

    Update plugin.py
    309

--- modbus-read/plugin.py
+++ modbus-read/plugin.py
@@ -306,7 +306,7 @@ class BasePlugin:
             if (Parameters["Mode5"] == "div10000"): value = str(round(value / 10000, 4))
             if (Parameters["Mode5"] == "div2"): value = str(round(value / 2, 2))
 		
-            if (value != "0"): Devices[1].Update(1, value) # Update value in Domoticz
+            if (value != "0"): Devices[1].Update(0, value) # Update value in Domoticz
             #Devices[1].Update(0, value) # Update value in Domoticz
 
           except:
@@ -315,7 +315,7 @@ class BasePlugin:
 		
     def UpdateDevice(Unit, nValue, sValue):
         # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
-        if (Unit in Devices):
+	if (Unit in Devices):
           if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
             Devices[Unit].Update(nValue, str(sValue))
             Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
